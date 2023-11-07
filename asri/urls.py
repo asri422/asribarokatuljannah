7e31372c81a27be194dddb09ee179e28c1a196b8
Asri Barokatul Jannah <asribarokatuljannah@gmail.com>
2023-11-07 11:36:53 +0700

    Menambah halaman penmu

--- asri/urls.py
+++ asri/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='asri'),
+    path('penmu', views.penmu, name='penmu'),
 ]
--- asri/views.py
+++ asri/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'asri/index.html')
+
+def penmu(request):
+    return render(request, 'asri/penmu.html')
